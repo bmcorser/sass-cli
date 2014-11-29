1b92c8fd2e932c5ed068e11e8f880d7fca2b0081
bmcorser <benmarshallcorser@gmail.com>
2014-11-29 19:44:29 +0000

    Test for other handled case, docstrings

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,10 +3,12 @@ from sass_cli import main
 
 
 def stripall(chars):
+    'Whitespace is not so relevant'
     return chars.replace('\n', '').replace(' ', '')
 
 
 def test_directories(output_dir):
+    'Works as expected when a pair of dirnames are passed'
     input_dir = 'tests/scss'
     expected_root = 'tests/css'
     main.directories(input_dir, output_dir)
@@ -21,3 +23,14 @@ def test_directories(output_dir):
             assert css == expected_css
             print("success: {0}".format(cssname))
             print('---')
+
+
+def test_files(output_dir):
+    'Works as expected when a pair of filenames are passed'
+    input_file = 'tests/scss/specific/c.scss'
+    output_file = os.path.join(output_dir, 'output.css')
+    expected_output_file = 'tests/css/specific/c.css'
+    main.compile_write(input_file, output_file)
+    css = stripall(open(output_file, 'r').read())
+    expected_css = stripall(open(expected_output_file, 'r').read())
+    assert css == expected_css
